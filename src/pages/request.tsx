be3608a95b26e4561c4fe52cb88303e1d68d46b2
nakaumin <nakaumin@gmail.com>
2024-02-24 17:08:02 +0900

    アカウントリクエストページ仮作成

---
+++ src/pages/request.tsx
@@ -0,0 +1,146 @@
+import { firstValueFrom } from "rxjs"
+import React, { useEffect, useState } from 'react'
+import LeftDrawer from '@/components/LeftDrawer'
+import Header from '@/components/Header'
+import { Box, Typography, Backdrop, CircularProgress } from '@mui/material'
+import {
+  Address,
+  Deadline,
+  UInt64,
+  Mosaic,
+  MosaicId,
+  PlainMessage,
+  Transaction,
+  TransferTransaction,
+} from 'symbol-sdk'
+
+import {
+  currencyMosaicID,
+  epochAdjustment,
+  networkType,
+} from '@/consts/blockchainProperty'
+
+import useSssInit from '@/hooks/useSssInit'
+import useAddressInit from '@/hooks/useAddressInit'
+
+import { useForm, SubmitHandler } from "react-hook-form"
+
+import { signTx } from '@/utils/signTx'
+
+function createAccountRequestTx(providerRawAddress: string, accountName: string, accountRawAddress: string): Transaction
+{
+  // Transaction info
+  const deadline = Deadline.create(epochAdjustment) // デフォルトは2時間後
+  const providerAddress = Address.createFromRawAddress(providerRawAddress)
+  const absoluteAmountUInt64 = UInt64.fromUint(0)
+  const mosaic = new Mosaic(new MosaicId(currencyMosaicID), absoluteAmountUInt64)
+  const mosaics = [mosaic]
+  const plainMessage = PlainMessage.create(accountName + ':' + accountRawAddress) // 平文メッセージに希望アカウント名とアドレスをエンコード
+  const feeMultiplier = 100
+
+  // Create transaction
+  const transferTransaction = TransferTransaction.create(
+    deadline,
+    providerAddress,
+    mosaics,
+    plainMessage,
+    networkType
+  ).setMaxFee(feeMultiplier)
+
+  return transferTransaction
+}
+
+function Request(): JSX.Element {
+
+  //SSS共通設定
+  const { clientPublicKey, sssState } = useSssInit()
+
+  // アドレス取得
+  const { address } = useAddressInit(clientPublicKey, sssState)
+
+  type Inputs = {
+    providerRawAddress: string
+    accountName: string
+    accountRawAddress: string
+  }
+
+  const {
+    register,
+    handleSubmit,
+  } = useForm<Inputs>()
+
+  // SUBMIT LOGIC
+  const requestAccount: SubmitHandler<Inputs> = (data) => {
+    signTx(
+      createAccountRequestTx(data.providerRawAddress, data.accountName, data.accountRawAddress)
+    )
+  }
+
+  //View共通設定
+  const [openLeftDrawer, setOpenLeftDrawer] = useState<boolean>(false) //LeftDrawerの設定
+  return (
+    <>
+      <Header setOpenLeftDrawer={setOpenLeftDrawer} />
+      <LeftDrawer openLeftDrawer={openLeftDrawer} setOpenLeftDrawer={setOpenLeftDrawer} />
+
+      {address === undefined ? (
+        <Backdrop open={address === undefined}>
+          <CircularProgress color='inherit' />
+        </Backdrop>
+      ) : (
+        <Box
+          p={3}
+          display='flex'
+          alignItems='center'
+          justifyContent='center'
+          flexDirection='column'
+        >
+          <Typography component='div' variant='h6' mt={5} mb={1}>
+            あなたのアドレス
+          </Typography>
+          { address.plain() }
+          <form onSubmit={handleSubmit(requestAccount)} className="m-4 px-8 py-4 border w-full max-w-80 flex flex-col gap-4">
+            <div className="flex flex-col">
+              <label>
+                アカウントプロバイダアドレス
+              </label>
+              <input
+                {...register("providerRawAddress", { required: "アドレスを入力してください" })}
+                className="rounded-md border px-3 py-2 focus:border-2 focus:border-teal-500 focus:outline-none"
+                type="text"
+                name="providerRawAddress"
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <label>
+                希望するアカウント名
+              </label>
+              <input
+                {...register("accountName", { required: "アドレスを入力してください" })}
+                className="rounded-md border px-3 py-2 focus:border-2 focus:border-teal-500 focus:outline-none"
+                type="text"
+                name="accountName"
+              />
+            </div>
+
+            <div className="flex flex-col">
+              <label>
+                割当先アドレス
+              </label>
+              <input
+                {...register("accountRawAddress", { required: "アドレスを入力してください" })}
+                className="rounded-md border px-3 py-2 focus:border-2 focus:border-teal-500 focus:outline-none"
+                type="text"
+                name="accountRawAddress"
+              />
+            </div>
+
+            <button>送信</button>
+          </form>
+        </Box>
+      )}
+    </>
+  )
+}
+export default Request

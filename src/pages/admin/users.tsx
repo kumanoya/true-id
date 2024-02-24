400cdc30166244558b779982ad1283c39c23a7e0
nakaumin <nakaumin@gmail.com>
2024-02-24 16:17:06 +0900

    仮にフォームの幅を広げた

--- src/pages/admin/users.tsx
+++ src/pages/admin/users.tsx
@@ -205,7 +205,7 @@ function Home(): JSX.Element {
             ユーザーID管理@{ parentNamespace }
           </Typography>
           { address.plain() }
-          <form onSubmit={handleSubmit(registerNamespace)} className="m-4 px-8 py-4 border w-full max-w-96 flex flex-col gap-4">
+          <form onSubmit={handleSubmit(registerNamespace)} className="m-4 px-8 py-4 border w-full max-w-120 flex flex-col gap-4">
             <div className="flex flex-col">
               <label> ID </label>
               <input

a0452cef3c0038a66fa191526f3b06aa4f7d50c0
nakaumin <nakaumin@gmail.com>
2024-03-14 00:10:40 +0900

    useAppAccountフック追加

---
+++ src/hooks/useAppAccount.ts
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import {
+  networkType,
+} from '@/consts/blockchainProperty'
+
+import {
+  Account,
+} from 'symbol-sdk'
+
+const useAppAccount = (): Account|undefined => {
+
+  const router = useRouter()
+
+  const [appAccount, setAppAccount] = useState<Account>()
+  useEffect(() => {
+    const pk = localStorage.getItem('appPrivateKey')
+    if (!pk || pk.length !== 64) {
+      alert('一般ユーザの秘密鍵が設定されていません')
+      router.push('/settings')
+      throw new Error('一般ユーザの秘密鍵が設定されていません')
+    }
+
+    const account = Account.createFromPrivateKey(pk as string, networkType)
+    setAppAccount(account)
+    if (account === undefined) {
+      alert('一般ユーザの秘密鍵が不正です')
+      router.push('/settings')
+      throw new Error('一般ユーザの秘密鍵が設定されていません')
+    }
+  }, [])
+
+  return appAccount
+}
+
+export default useAppAccount
+
+

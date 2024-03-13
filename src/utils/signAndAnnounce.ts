80209437dd9f7b1c49d9a1224d0b57acc4cf98e9
nakaumin <nakaumin@gmail.com>
2024-03-13 22:01:07 +0900

    signAndAnnounceにsignerを指定可能に

--- src/utils/signAndAnnounce.ts
+++ src/utils/signAndAnnounce.ts
@@ -1,6 +1,7 @@
 import {
   SignedTransaction,
   Transaction,
+  Account,
 } from 'symbol-sdk'
 import { createRepositoryFactory } from '@/utils/createRepositoryFactory'
 
@@ -14,17 +15,25 @@ interface SSSWindow extends Window {
 }
 declare const window: SSSWindow
 
-export const signTx = async (tx: Transaction): Promise<any> => {
-  window.SSS.setTransaction(tx)
-  //console.log(tx)
-  const signedTx: SignedTransaction = await new Promise((resolve) => {
-    resolve(window.SSS.requestSign())
-  })
+export const signTx = async (tx: Transaction, signer: Account | null = null): Promise<any> => {
+
+  let signedTx: SignedTransaction
+
+  if (signer) {
+    const generationHash = await repo.getGenerationHash().toPromise()
+    signedTx = signer.sign(tx, generationHash as string)
+  } else {
+    window.SSS.setTransaction(tx)
+    signedTx = await new Promise((resolve) => {
+      resolve(window.SSS.requestSign())
+    })
+  }
+
   return signedTx
 }
 
-export const signAndAnnounce = async (tx: Transaction): Promise<any> => {
-  const signedTx = await signTx(tx)
+export const signAndAnnounce = async (tx: Transaction, signer: Account | null = null): Promise<any> => {
+  const signedTx = await signTx(tx, signer)
   const ret = txRepo.announce(signedTx)
   if (!ret) {
     throw new Error('Transaction not announced')

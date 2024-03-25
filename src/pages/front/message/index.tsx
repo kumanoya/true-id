3b3ad0dea8452053da9aaef91faaef2ad762185b
nakaumin <nakaumin@gmail.com>
2024-03-26 08:10:49 +0900

    messageページの不要コード削除

--- src/pages/front/message/index.tsx
+++ src/pages/front/message/index.tsx
@@ -1,48 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import FrontLayout from '@/components/FrontLayout';
 import styles from './message.module.css'
-import {
-  Transaction,
-  TransactionGroup,
-  Order,
-} from 'symbol-sdk';
-
-import useSssInit from '@/hooks/useSssInit';
-import useAddressInit from '@/hooks/useAddressInit';
-
-import { format } from "date-fns";
 
 const messages = [
   { sender: "Alice", content: "こんにちは！", time: "10:00", isOwn: false },
   { sender: "Bob", content: "こんにちは、Alice！", time: "10:01", isOwn: true },
 ];
 
-function formatTimestamp(timestamp: { lower: number; higher: number }): string {
-  // UNIXタイムスタンプをミリ秒単位で計算
-  const unixTimestamp = (timestamp.higher * 4294967296 + timestamp.lower) / 1000;
-
-  // Dateオブジェクトを生成
-  const date = new Date(unixTimestamp * 1000);
-
-  // date-fnsを使ってフォーマット
-  return format(date, "yyyy-MM-dd HH:mm");
-}
-
 function Message(): JSX.Element {
-
-  //SSS共通設定
-  const { clientPublicKey, sssState } = useSssInit();
-
-  // アドレス取得
-  const { address } = useAddressInit(clientPublicKey, sssState);
-
-  useEffect(() => {
-    if (sssState === 'ACTIVE' && address !== undefined) {
-      (async() => {
-
-      })();
-    }
-  },  [address, sssState]);
 
   const [message, setMessage] = useState('');
 

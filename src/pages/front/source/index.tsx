7d77cc33c52e1f6866563bf6f60b0836a4d60ebe
nakaumin <nakaumin@gmail.com>
2024-03-26 02:27:53 +0900

    sourceページをuseUserAccountを利用するように修正

--- src/pages/front/source/index.tsx
+++ src/pages/front/source/index.tsx
@@ -1,49 +1,52 @@
-import { firstValueFrom } from "rxjs";
-import React, { useEffect, useState } from 'react';
-import FrontLayout from '@/components/FrontLayout';
-import { Typography, List, ListItem, ListItemAvatar, Avatar, ListItemText, Divider } from '@mui/material';
+import { firstValueFrom } from "rxjs"
+import React, { useEffect, useState } from 'react'
+import FrontLayout from '@/components/FrontLayout'
+import { Typography, List, ListItem, ListItemAvatar, Avatar, ListItemText, Divider } from '@mui/material'
 import {
   Transaction,
   TransactionGroup,
   Order,
-} from 'symbol-sdk';
+} from 'symbol-sdk'
 
-import useSssInit from '@/hooks/useSssInit';
-import useAddressInit from '@/hooks/useAddressInit';
+//import { useUserInfo } from '@/store/UserInfoContext'
+import useUserAccount from '@/hooks/useUserAccount'
 
-import { createRepositoryFactory } from '@/utils/createRepositoryFactory';
-import { format } from "date-fns";
-import { useRouter } from 'next/router';
+import { createRepositoryFactory } from '@/utils/createRepositoryFactory'
+import { format } from "date-fns"
+import { useRouter } from 'next/router'
 
-const repo = createRepositoryFactory();
+const repo = createRepositoryFactory()
 
 function formatTimestamp(timestamp: { lower: number; higher: number }): string {
   // UNIXタイムスタンプをミリ秒単位で計算
-  const unixTimestamp = (timestamp.higher * 4294967296 + timestamp.lower) / 1000;
+  const unixTimestamp = (timestamp.higher * 4294967296 + timestamp.lower) / 1000
 
   // Dateオブジェクトを生成
-  const date = new Date(unixTimestamp * 1000);
+  const date = new Date(unixTimestamp * 1000)
 
   // date-fnsを使ってフォーマット
-  return format(date, "yyyy-MM-dd HH:mm");
+  return format(date, "yyyy-MM-dd HH:mm")
 }
 
+//==============================================================================
+//  Source
+//==============================================================================
 function Source(): JSX.Element {
-  const router = useRouter();
-
-  //SSS共通設定
-  const { clientPublicKey, sssState } = useSssInit();
+  const router = useRouter()
 
   // アドレス取得
-  const { address } = useAddressInit(clientPublicKey, sssState);
+  //const { account } = useUserInfo()
+  const userAccount = useUserAccount()
 
   // メッセージ一覧表示用
-  const [dataList, setDataList] = useState<Transaction[]>([]);
+  const [dataList, setDataList] = useState<Transaction[]>([])
 
   useEffect(() => {
-    if (sssState === 'ACTIVE' && address !== undefined) {
+    console.log('Account: ', userAccount)
+    if (userAccount) {
       (async() => {
-        const txRepo = repo.createTransactionRepository();
+        const address = userAccount.address
+        const txRepo = repo.createTransactionRepository()
 
         const resultSearch = await firstValueFrom(
           txRepo.search({            // type: [TransactionType.AGGREGATE_BONDED],
@@ -52,47 +55,47 @@ function Source(): JSX.Element {
             order: Order.Desc,
             pageSize: 100,
           })
-        );
-        console.log('resultSearch :', resultSearch);
-        // setDataList(resultSearch.data);
+        )
+        console.log('resultSearch :', resultSearch)
+        // setDataList(resultSearch.data)
 
         // データをグループ化し、各グループで最新のトランザクションを保持する
         const grouped: { [key: string]: Transaction } = resultSearch.data.reduce((tx, current) => {
-          const address = current.signer?.address?.plain();
+          const address = current.signer?.address?.plain()
           if (address && ((!tx[address]) ||
             (tx[address]?.transactionInfo?.height ?? 0) < (current?.transactionInfo?.timestamp ?? 0))
           ) {
-            tx[address] = current;
+            tx[address] = current
           }
-          return tx;
-        }, {} as { [key: string]: Transaction });
+          return tx
+        }, {} as { [key: string]: Transaction })
 
-        const filteredDataList = Object.values(grouped);
+        const filteredDataList = Object.values(grouped)
         const sortedDataList = [...filteredDataList].sort((a, b) => {
-          return Number(b.transactionInfo?.timestamp ?? 0) - Number(a.transactionInfo?.timestamp ?? 0);
-        });
-        setDataList(sortedDataList);
+          return Number(b.transactionInfo?.timestamp ?? 0) - Number(a.transactionInfo?.timestamp ?? 0)
+        })
+        setDataList(sortedDataList)
 
-        console.log('filteredDataList ⚡️', filteredDataList);
+        console.log('filteredDataList ⚡️', filteredDataList)
 
         // Start monitoring of transaction status with websocket
-        const listener = repo.createListener();
-        await listener.open();
+        const listener = repo.createListener()
+        await listener.open()
         listener
           .confirmed(address)
           .subscribe((confirmedTx: Transaction) => {
-            console.log("EVENT: TRANSACTION CONFIRMED");
-            //console.dir({ confirmedTx }, { depth: null });
-            setDataList(current => [confirmedTx, ...current]);
-          });
-      })();
+            console.log("EVENT: TRANSACTION CONFIRMED")
+            //console.dir({ confirmedTx }, { depth: null })
+            setDataList(current => [confirmedTx, ...current])
+          })
+      })()
     }
-  },  [address, sssState]);
+  },  [userAccount])
 
   // TODO: 現在は仮。適切なルーティング設定を行う
   const handleNavigate = () => {
-    router.push('/');
-  };
+    router.push('/')
+  }
 
   return (
     <FrontLayout>
@@ -132,6 +135,6 @@ function Source(): JSX.Element {
       </List>
 
     </FrontLayout>
-  );
+  )
 }
-export default Source;
+export default Source

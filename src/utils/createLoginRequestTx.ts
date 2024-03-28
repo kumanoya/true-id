4c514160bc2811b4dbac4df585bf78b8440d022d
nakaumin <nakaumin@gmail.com>
2024-03-29 07:34:05 +0900

    ログインリクエストの送信先の誤り修正、idを@形式に

--- src/utils/createLoginRequestTx.ts
+++ src/utils/createLoginRequestTx.ts
@@ -3,23 +3,23 @@ import {
   UInt64,
   Mosaic,
   MosaicId,
-  NamespaceId,
+  Address,
   PlainMessage,
   Transaction,
   TransferTransaction,
 } from 'symbol-sdk'
-
 import {
   loginRequestMosaicId,
   epochAdjustment,
   networkType,
 } from '@/consts/blockchainProperty'
+import namespaceToRawAddress from '@/utils/namespaceToRawAddress'
+import { unformatId } from '@/utils/formatId'
 
-import { createRepositoryFactory } from '@/utils/createRepositoryFactory';
-const repo = createRepositoryFactory();
-
-async function createLoginRequestTx(accountName: string): Promise<Transaction>
+async function createLoginRequestTx(userId: string): Promise<Transaction>
 {
+  const id = unformatId(userId)
+
   // Transaction info
   const deadline = Deadline.create(epochAdjustment) // デフォルトは2時間後
 
@@ -27,22 +27,19 @@ async function createLoginRequestTx(accountName: string): Promise<Transaction>
   const absoluteAmountUInt64 = UInt64.fromUint(0)
   const mosaic = new Mosaic(new MosaicId(loginRequestMosaicId), absoluteAmountUInt64)
   const mosaics = [mosaic]
-  const plainMessage = PlainMessage.create(accountName)
+  const plainMessage = PlainMessage.create(id)
   const feeMultiplier = 100
 
   // 宛先アドレスを取得
-  // namespace宛に送ると受信側での受信処理が複雑になる(アドレスで絞り込みできない)のでaddressで送る
-  const namespaceId = new NamespaceId(accountName)
-  const namespaceInfo = await repo.createNamespaceRepository().getNamespace(namespaceId).toPromise()
-  if (!namespaceInfo) {
-    throw new Error('Invalid root namespace')
+  const recipientRawAddress = await namespaceToRawAddress(id)
+  if (recipientRawAddress === null) {
+    throw new Error('Recipient address is invalid')
   }
-  const recipientAddress = namespaceInfo.ownerAddress
 
   // Create transaction
   const transferTransaction = TransferTransaction.create(
     deadline,
-    recipientAddress, //namespaceId,
+    Address.createFromRawAddress(recipientRawAddress),
     mosaics,
     plainMessage,
     networkType

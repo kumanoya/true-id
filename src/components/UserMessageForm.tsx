f26f4caba8fec0e9f4980533607e81b8452d6914
nakaumin <nakaumin@gmail.com>
2024-03-29 21:25:18 +0900

    UserMessageFormのcreateMessageTxの冗長な定義廃止

--- src/components/UserMessageForm.tsx
+++ src/components/UserMessageForm.tsx
@@ -1,59 +1,7 @@
-import { Typography } from '@mui/material'
-import {
-  Deadline,
-  UInt64,
-  Mosaic,
-  MosaicId,
-  NamespaceId,
-  PlainMessage,
-  Transaction,
-  TransferTransaction,
-} from 'symbol-sdk'
-import {
-  currencyMosaicID,
-  epochAdjustment,
-  networkType,
-} from '@/consts/blockchainProperty'
 import { useForm, SubmitHandler } from "react-hook-form"
 import { signAndAnnounce } from '@/utils/signAndAnnounce'
-import { unformatId } from '@/utils/formatId'
 import { useUserInfo } from '@/store/UserInfoContext'
-
-function createMessageTx(recipientId: string, rawMessage: string, xym: number, currentUserId: string|null = null): Transaction
-{
-  // XXX: ハードコード
-  const networkCurrencyDivisibility = 6 // XYMの分割単位
-
-  // Transaction info
-  const deadline = Deadline.create(epochAdjustment) // デフォルトは2時間後
-  const recipientNamespaceId = new NamespaceId(recipientId)
-
-  const absoluteAmount =
-    xym * parseInt("1" + "0".repeat(networkCurrencyDivisibility)) // networkCurrencyDivisibility = 6 => 1[XYM] = 10^6[μXYM]
-  const absoluteAmountUInt64 = UInt64.fromUint(absoluteAmount)
-  const mosaic = new Mosaic(new MosaicId(currencyMosaicID), absoluteAmountUInt64)
-  const mosaics = [mosaic]
-
-  const message = {
-    recipientId: recipientId,
-    content: rawMessage,
-    signerId: currentUserId,
-  }
-
-  const plainMessage = PlainMessage.create(JSON.stringify(message)) // 平文メッセージ
-  const feeMultiplier = 100
-
-  // Create transaction
-  const transferTransaction = TransferTransaction.create(
-    deadline,
-    recipientNamespaceId,
-    mosaics,
-    plainMessage,
-    networkType
-  ).setMaxFee(feeMultiplier)
-
-  return transferTransaction
-}
+import createMessageTx from '@/utils/createMessageTx'
 
 interface Props { recipientId: string }
 function UserMessageForm({ recipientId }: Props): JSX.Element {

1a76403b5a259c8ae8e4affcb67b1939a4fc39ea
nakaumin <nakaumin@gmail.com>
2024-03-31 04:29:19 +0900

    接続先ノードを変更

--- src/utils/createRepositoryFactory.ts
+++ src/utils/createRepositoryFactory.ts
@@ -3,7 +3,10 @@ import {
 } from 'symbol-sdk';
 
 export const createRepositoryFactory = () => {
-  const NODE = "https://sym-test-03.opening-line.jp:3001";
+  //const NODE = "https://sym-test-01.opening-line.jp:3001";
+  //const NODE = "https://sym-test-03.opening-line.jp:3001";
+  //const NODE = 'https://test01.xymnodes.com:3001'
+  const NODE = 'https://test02.xymnodes.com:3001'
   const repo = new RepositoryFactoryHttp(NODE, {
     websocketUrl: NODE.replace('http', 'ws') + '/ws',
     websocketInjected: WebSocket,
